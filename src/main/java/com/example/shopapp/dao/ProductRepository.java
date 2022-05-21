462d91996df192897d7d676df6338c2c03772583
Marcin Lamek <fluffgit@gmail.com>
2022-05-21 15:23:37 +0200

    commit

--- src/main/java/com/example/shopapp/dao/ProductRepository.java
+++ src/main/java/com/example/shopapp/dao/ProductRepository.java
@@ -1,2 +1,10 @@
-package com.example.shopapp.dao;public interface ProductRepository {
+package com.example.shopapp.dao;
+
+import com.example.shopapp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
